72391a0b7bb169efaef8ecb2b54e092216730896
Sofie Erner <sofie@erner.dk>
2023-07-26 11:22:38 +0100

    Added Dark matter UFO model

---
+++ Dark_Matter_Model/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.47
+# Mathematica version: 13.2.1 for Linux x86 (64-bit) (January 27, 2023)
+# Date: Wed 26 Jul 2023 11:21:42
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ Dark_Matter_Model/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.3.47
+# Mathematica version: 13.2.1 for Linux x86 (64-bit) (January 27, 2023)
+# Date: Wed 26 Jul 2023 11:21:42
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+HIW = CouplingOrder(name = 'HIW',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
